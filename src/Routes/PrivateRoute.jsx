e27987305e61f06b50752b9ff47e11863d02261b
slowandrarecooked <airbendervineeth@gmail.com>
2023-09-08 23:56:37 +0530

    v1

--- src/Routes/PrivateRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-export const PrivateRoute = ({ children }) => {
-  let { loggedIn } = useSelector((store) => store.reducer);
-  if (!loggedIn) return <Navigate to="/Login" />;
-  return children;
-};
